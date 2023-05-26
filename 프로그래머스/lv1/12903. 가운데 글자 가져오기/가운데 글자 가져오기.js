64a59c6d7e1fa5266674fb161f993a18cffa1488
Dalso13 <119721317+Dalso13@users.noreply.github.com>
2023-05-26 15:11:40 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    var answer = '';
+	
+		if (s.length % 2 == 0){
+			answer =  s.substring((s.length/2-1),(s.length/2)+1);
+		} else {
+			answer = s.substring((s.length/2),(s.length/2)+1);
+		}
+        
+   
+    
+    return answer;
+ 
+}
