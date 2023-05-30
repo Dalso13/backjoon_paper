0dc92b55299b2efd822c654473663415e7b75794
Dalso13 <119721317+Dalso13@users.noreply.github.com>
2023-05-30 12:03:41 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,4 @@
+-- 코드를 입력하세요 
+SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE from ANIMAL_INS
+where name = 'Lucy' or name = 'Ella' or name = 'Pickle' or name = 'rogan' or name = 'sabrina' or name = 'mitty'
+
