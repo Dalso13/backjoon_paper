e97eb7f19ae28c94b87d3972911a6c1a86622409
Dalso13 <119721317+Dalso13@users.noreply.github.com>
2023-05-30 12:27:24 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,13 @@
+function solution(price, money, count) {
+    var answer = 0;
+    
+    for (let i = 1 ; i <= count ; i++) {
+        answer += price * i
+    }
+    if(answer <= money) {
+        return 0;
+    }else {
+        return answer - money;
+    }
+    
+}
