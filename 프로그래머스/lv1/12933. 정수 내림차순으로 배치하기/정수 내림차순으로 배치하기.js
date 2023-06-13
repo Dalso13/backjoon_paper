87c4720a475b4a840c60f80eb0bdf8f45ed4c854
Dalso13 <119721317+Dalso13@users.noreply.github.com>
2023-06-13 17:42:28 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    n = n+"";
+    
+    n = n;
+    
+    n = n.split("").sort(function(a , b) {
+	        return b-a;
+        }).join(",").replaceAll(",","");
+    
+
+    var answer = n/1;
+    return answer;
+}
